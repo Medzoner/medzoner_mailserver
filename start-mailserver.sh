3b4d7c41f1de1741cf39388b5d7f4c52a9b508bd
medzoner <medzoner@medzoner.com>
2017-04-17 17:18:30 +0200

    fix services

--- start-mailserver.sh
+++ start-mailserver.sh
@@ -4,18 +4,18 @@
 
 
 cron
-/etc/init.d/rsyslog start
-/etc/init.d/saslauthd start
-/etc/init.d/fail2ban start
-/etc/init.d/opendkim start
-/etc/init.d/opendmarc start
-/etc/init.d/postfix start
+service rsyslog start
+service saslauthd start
+service fail2ban start
+service opendkim start
+service opendmarc start
+service postfix start
 
-/usr/sbin/dovecot -c /etc/dovecot/dovecot.conf
-/usr/sbin/dovecot reload
+dovecot -c /etc/dovecot/dovecot.conf
+dovecot reload
 
 freshclam
-/etc/init.d/fetchmail start
-/etc/init.d/clamav-daemon start
-/etc/init.d/postgrey start
-/etc/init.d/amavis start
+service fetchmail start
+service clamav-daemon start
+service postgrey start
+service amavis start
